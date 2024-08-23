d18057ceace11b7ee9982fd248d5b871109164e8
Gamze Şirin <gamze1sirin@gmail.com>
2024-08-23 11:04:14 +0300

    project and skills created.

---
+++ components/content/Projects.tsx
@@ -0,0 +1,14 @@
+import { Card } from './Card'
+import React from 'react'
+import SparklesText from '../magicui/sparkles-text'
+
+const Projects = () => {
+	return (
+		<div className=" -mt-7" id="projects">
+			<SparklesText text="Projelerim" />
+			<Card />
+		</div>
+	)
+}
+
+export default Projects
